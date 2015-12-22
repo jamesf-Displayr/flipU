435931144fc5697180ee326c75f5118bc71c29ad
TimBock <tim.bock@numbers.net.au>
2015-12-22 11:04:20 +1100

    Flip U initial stuff

---
+++ R/utilities.R
@@ -0,0 +1,25 @@
+#'  \code{IfThen}
+#' @description If else statment as a single line.
+#' @param condition A logical expression.
+#' @param TRUE.result The object to be returned if \code{condition} is \code{TRUE}.
+#' @param FALSE.result The object to be returned if \code{condition} is \code{FALSE}.
+IfThen <- function(condition, TRUE.result, FALSE.result)
+{
+    if(condition)
+        return(TRUE.result);
+    return(FALSE.result);
+}
+
+#' \code{RemoveRowsAndOrColumns}
+#' @description Removes rows or columns from the table.
+#' @param x The data that is being analyzed
+#' @param row.names.to.remove A vector of the row labels to remove.
+#' @param column.names.to.remove A vector of the column labels to remove.
+#' @export
+RemoveRowsAndOrColumns = function(x,
+        row.names.to.remove = c("NET", "Total", "SUM"),
+        column.names.to.remove = c("NET", "Total", "SUM"))
+{
+    x[!rownames(x) %in% row.names.to.remove, !colnames(x) %in% column.names.to.remove ]
+}
+
