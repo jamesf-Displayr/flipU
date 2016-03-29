3024cb36d1fea35b7c245915c161da87a2821c80
TimBock <tim.bock@numbers.net.au>
2016-03-29 13:43:05 +1100

    Adding GetTidyTwoDimensionalArray

--- R/utilities.R
+++ R/utilities.R
@@ -11,6 +11,44 @@ IfThen <- function(condition, TRUE.result, FALSE.result)
     return(FALSE.result);
 }
 
+
+
+#' \code{GetTidyTwoDimensionalArray}
+#' @description Checks that an array is two dimensional and tidies if appropriate (assuming it is a Q table).
+#' @param x The data that is being analyzed
+#' @param row.names.to.remove A vector of the row labels to remove.
+#' @param column.names.to.remove A vector of the column labels to remove.
+#' @export
+GetTidyTwoDimensionalArray <- function(x, row.names.to.remove, column.names.to.remove)
+{
+    dim.x <- dim(x)
+    dim.names <- dimnames(x)
+    if (length(dim.x) != 2)
+    {
+        if (length(dim.x) == 3 & !is.null(dim.names))
+        {
+            x <- x[ , ,1]
+            warning(paste0("Correspondence analysis has been performed on the first statistic in the table (",
+                           dim.names[[3]][1], ")."))
+            if (is.character(x[1,1]))
+                x <- matrix(as.numeric(x), nrow(x), dimnames = dimnames(x))
+        }
+        else
+        {
+            stop("Correspondence analysis can only be peformed with a two-dimensional table (i.e., a table with one set of row headings, one set of columns headings, and one statistic in each cell.")
+        }
+    }
+    if (is.null(dim.names))
+    {
+        dimnames(x) <- list(Rows = 1:nrow(x), Columns = 1:ncol(x))
+    }
+    else
+    {
+        x <- RemoveRowsAndOrColumns(x, row.names.to.remove, column.names.to.remove)
+    }
+    x
+}
+
 #' \code{RemoveRowsAndOrColumns}
 #' @description Removes rows or columns from the table.
 #' @param x The data that is being analyzed
