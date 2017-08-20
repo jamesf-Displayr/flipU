ef468d27bbf28528a48b639ffbd8e8da5a574bae
tim.bock@numbers.net.au <tim.bock@numbers.net.au>
2017-08-20 13:43:25 +1000

    LookupName

---
+++ R/lookups.R
@@ -0,0 +1,21 @@
+#' LookupName
+#'
+#' @param value The value to be look uped in a \code{dictionary}.
+#' @param dictionary A \code{\link{vector}} of values.
+#' @param details Returns the name of the element in a vector corresponding to the \code{value}.
+#' Where the vector is un-named, its index is
+#' returned. Where the value appears multiple times a warning is provided. Returns a \code{NULL} if
+#' the value is not found.
+#' @export
+LookupName <- function(value, dictionary)
+{
+    if (is.null(names(dictionary)))
+        names(dictionary) <- 1:length(dictionary)
+    matches <- dictionary %in% value
+    if (all(!matches))
+        return(NULL)
+    nms <- names(dictionary)[matches]
+    if (length(nms) > 1)
+        warning(paste0("''", value, "'appears in the dictionnary multiple times. The first has been used."))
+    nms[1]
+}
---
+++ tests/testthat/test-lookups.R
@@ -0,0 +1,11 @@
+context("lookups")
+
+
+test_that("LookupName",{
+    d <- c("A" = 1, "B" = 2, "C" = 1)
+    expect_equal(LookupName(2, d), "B")
+    expect_equal(suppressWarnings(LookupName(1, d)), "A")
+    expect_warning(LookupName(1, d))
+    expect_equal(LookupName(3, d), NULL)
+
+})
