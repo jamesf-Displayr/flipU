1cb831a9130a880ac23a878f750c006233f44961
JustinCCYap <justinyapcc@gmail.com>
2017-12-19 12:06:05 +1100

    Improve documentation for warnings functions

--- R/warnings.R
+++ R/warnings.R
@@ -1,17 +1,35 @@
-#' \code{ExpectWarning}
-#' @description Checks to see if the printing of an object causes warnings to appear.
+#' @title ExpectWarning
+#' @description Checks to see if the printing of an object causes warnings to
+#' appear.
 #' @param code The code to evaluate for warnings.
-#' @param string The string of text to be searched for in the warnings
+#' @param string The string of text to be searched for in the warnings.
+#' @return Returns NULL. Throws an exception if the code is evaluated and no
+#' warning is thrown or the string does not appear in the warning.
+#' @examples
+#' throwsAWarning <- function()
+#' {
+#'     warning("This is a warning")
+#' }
+#' ExpectWarning(throwsAWarning(), "a warning")
 #' @export
 ExpectWarning <- function(code, string)
 {
     stopifnot(containsWarning(code, string))
 }
 
-#' \code{ExpectNoWarning}
-#' @description Checks to see if the printing of an object does not cause a warning to appear.
+#' @title ExpectNoWarning
+#' @description Checks to see if the printing of an object does not cause a
+#' warning to appear.
 #' @param code The code to evaluate for warnings.
 #' @param string The string of text to be searched for in the warnings
+#' @return Returns NULL. Throws an exception if the code is evaluated and
+#' the string appears in the warning.
+#' @examples
+#' throwsAWarning <- function()
+#' {
+#'     warning("This is a warning")
+#' }
+#' ExpectNoWarning(throwsAWarning(), "different warning")
 #' @export
 ExpectNoWarning <- function(code, string)
 {
@@ -24,13 +42,24 @@ containsWarning <- function(code, string)
     any(grepl(string, my.warnings))
 }
 
-
 #' @title InterceptWarnings
 #' @description This function intercepts warning messages produced from running
 #' \code{expr} and passes them to \code{warning.handler}.
 #' @param expr The expression whose warnings are to be intercepted.
 #' @param warning.handler The function that handles intercepted warnings.
 #' @return The value of \code{expr}.
+#' @examples
+#' throwsAWarning <- function()
+#' {
+#'     warning("This is a warning")
+#' }
+#' addExclamationMark <- function(warn)
+#' {
+#'     warning(paste0(warn$message, "!"))
+#' }
+#' ## Intercepts the warning from throwsAWarning and rethrows it with an
+#' ## exclamation mark.
+#' InterceptWarnings(throwsAWarning(), addExclamationMark)
 #' @export
 InterceptWarnings <- function(expr, warning.handler)
 {
