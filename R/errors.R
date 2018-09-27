6ead7551b77ce2c0f4a5c8cd6c1085f0479f5b8f
Justin Yap <justinyapcc@gmail.com>
2018-09-27 16:25:25 +1000

    Added function to check that variables from a form.dropBox control with multiple inputs have the same length

---
+++ R/errors.R
@@ -0,0 +1,14 @@
+#' @title CheckVariableLengths
+#' @param variable.list A list of variables from a form.dropBox control with
+#'     multiple inputs.
+#' @param list.name The name used to refer to the variable list in the error
+#'     message.
+#' @return Nothing is returned, but an error is thrown if the variables have
+#'     differing lengths.
+#' @export
+CheckVariableLengths <- function(variable.list, list.name)
+{
+    if (length(unique(sapply(inputVariables, length))) > 1)
+        stop("The ", list.name , " have differing lengths. Please ensure ",
+             "that they are from the same data set or have the same length.")
+}
