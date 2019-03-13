1c7e8b0c7d90d97fa3c9671d51026ab2712e9074
chschan <chschan@gmail.com>
2019-03-13 13:40:55 +1100

    Also move IsTestRServer from flipChoice

--- R/isrserver.R
+++ R/isrserver.R
@@ -9,3 +9,14 @@ IsRServer <- function()
         grepl("^reustest.*", node.name) ||
         grepl("^reusprod.*", node.name)
 }
+
+#' @title IsTestRServer
+#' @description This function indicates if it is being run on the test R
+#' server.
+#' @return TRUE if running on the test R server. False otherwise.
+#' @export
+IsTestRServer <- function()
+{
+    node.name <- Sys.info()[["nodename"]]
+    grepl("^reustest.*", node.name)
+}
