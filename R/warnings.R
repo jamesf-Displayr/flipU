5ab07bffc1e9e5497e517d473e557b1078e6d416
chrisfacer <chris.facer@q-researchsoftware.com>
2023-10-13 13:45:55 +1100

    Fixed if statement

--- R/warnings.R
+++ R/warnings.R
@@ -135,7 +135,7 @@ WarnIfVariablesSelectedFromMultipleDataSets <- function() {
     form.controls <- ls(pattern = "^form", envir = .GlobalEnv)
     all.form.selections <- lapply(form.controls, FUN = get0)
     all.data.sets.referenced <- unique(unlist(lapply(all.form.selections, getDataFileNameFromDisplayrObject)))
-    if (length(all.data.sets.referenced)) {
+    if (length(all.data.sets.referenced) > 1) {
         warning("The selected data come from more than one Data Set. ",
             "The data sets may have different lengths, and the cases ",
             "may not be in the same order. The data sets used are: ",
